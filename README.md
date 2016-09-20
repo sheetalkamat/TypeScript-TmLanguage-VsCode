57f3a5d520d47216ab8eb10101acce6def5e7c77
Sheetal Nandi <shkamat@microsoft.com>
2016-09-20 09:32:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## The Beta Version of the TypeScript Grammar 
+ 
+We use this extension to distribute the Beta version of our new TypeScript grammar. Please file
+any issues you find against https://github.com/Microsoft/TypeScript-TmLanguage
+ 
+### License
+The extension is made available under the following [Microsoft Software License Terms](LICENSE.txt).
+
---
+++ build/update-grammar.js
@@ -0,0 +1,116 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var plist = require('fast-plist');
+var https = require('https');
+var url = require('url');
+
+function adaptToJavaScript(grammar) {
+	grammar.name = 'JavaScript (with React support)';
+	grammar.fileTypes = ['.js', '.jsx' ];
+	grammar.scopeName = 'source.js';
+
+	var fixScopeNames = function(rule) {
+		if (typeof rule.name === 'string') {
+			rule.name = rule.name.replace(/\.tsx/g, '.js');
+		}
+		for (var property in rule) {
+			var value = rule[property];
+			if (typeof value === 'object') {
+				fixScopeNames(value);
+			}
+		}
+	};
+
+	var repository = grammar.repository;
+	for (var key in repository) {
+		fixScopeNames(repository[key]);
+	}
+	// disable type parameters
+	if (repository['type-parameters']) {
+		repository['type-parameters']['begin'] = 'DO_NOT_MATCH';
+	}
+}
+
+
+function getCommitSha(repoPath) {
+	var commitInfo = 'https://api.github.com/repos/Microsoft/TypeScript-TmLanguage/commits?path=' + repoPath;
+	return download(commitInfo).then(function (content) {
+		try {
+			let lastCommit = JSON.parse(content)[0];
+			return Promise.resolve({
+				commitSha: lastCommit.sha,
+				commitDate: lastCommit.commit.author.date
+			});
+		} catch (e) {
+			return Promise.resolve(null);
+		}
+	}, function () {
+		console.err('Failed loading ' + commitInfo);
+		return Promise.resolve(null);
+	});
+}
+
+function download(urlString) {
+	return new Promise((c, e) => {
+		var _url = url.parse(urlString);
+		var options = { host: _url.host, port: _url.port, path: _url.path, headers: { 'User-Agent': 'NodeJS' }};
+		var content = '';
+		var request = https.get(options, function (response) {
+			response.on('data', function (data) {
+				content += data.toString();
+			}).on('end', function () {
+				c(content);
+			});
+		}).on('error', function (err) {
+			e(err.message);
+		});
+	});
+}
+
+exports.update = function (branchId, repoPath, dest, modifyGrammar) {
+	var contentPath = 'https://raw.githubusercontent.com/Microsoft/TypeScript-TmLanguage/' + branchId + '/' + repoPath;
+	console.log('Reading from ' + contentPath);
+	return download(contentPath).then(function (content) {
+		var ext = path.extname(repoPath);
+		var grammar;
+		if (ext === '.tmLanguage' || ext === '.plist') {
+			grammar = plist.parse(content);
+		} else {
+			console.error('Unknown file extension: ' + ext);
+			return;
+		}
+		if (modifyGrammar) {
+			modifyGrammar(grammar);
+		}
+		return getCommitSha(repoPath).then(function (info) {
+			if (info) {
+				grammar.version = 'https://github.com/Microsoft/TypeScript-TmLanguage/commit/' + info.commitSha;
+			}
+
+			try {
+				fs.writeFileSync(dest, JSON.stringify(grammar, null, '\t'));
+				if (info) {
+					console.log('Updated ' + path.basename(dest) + ' to Microsoft/TypeScript-TmLanguage@' + info.commitSha.substr(0, 7) + ' (' + info.commitDate.substr(0, 10) + ')');
+				} else {
+					console.log('Updated ' + path.basename(dest));
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		});
+
+	}, console.error);
+}
+
+if (path.basename(process.argv[1]) === 'update-grammar.js') {
+	exports.update(process.argv[2], 'TypeScript.tmLanguage', process.argv[3] + 'TypeScript.tmLanguage.json');
+	exports.update(process.argv[2], 'TypeScriptReact.tmLanguage', process.argv[3] + 'TypeScriptReact.tmLanguage.json');
+	exports.update(process.argv[2], 'TypeScriptReact.tmLanguage', process.argv[3] + 'JavaScript.tmLanguage.json', adaptToJavaScript);
+}
